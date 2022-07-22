8d3612f7af4d469cdc4b7eda72ae55699ea070e0
Boris Knezevic <boris.knezevic0@gmail.com>
2022-07-22 11:04:49 +0200

    add csv recap

---
+++ lectures/todo_manager/csv.rb
@@ -0,0 +1,13 @@
+require 'csv'
+
+# PARSE CSV
+# CSV.foreach("task.csv") do |row|
+#   # We want to create instances of our model
+#   task = Task.new(row[0])
+#   @tasks << task
+# end
+
+# # STORE CSV
+# CSV.open("task.csv", "wb") do |csv|
+#   csv << # Push task.name to csv
+# end
